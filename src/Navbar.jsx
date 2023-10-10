985c8a4317c4661a6d023323edd09f97948a42c0
RedJyu <piotr.stezycki@protonmai.com>
2023-10-10 13:38:43 +0200

    toggle button

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,29 +4,35 @@ import { FaBars } from 'react-icons/fa';
 import logo from './logo.svg';
 
 function Navbar() {
+  const toggle = () => {
+    setShowLinks(!showLinks);
+  };
+
   const [showLinks, setShowLinks] = useState(false);
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} className='logo' alt='logo' />
-          <button className='nav-toggle'>
+          <button className='nav-toggle' onClick={toggle}>
             <FaBars />
           </button>
         </div>
-        {/* links-container class is important for it to work */}
-        <div className='links-container'>
-          <ul className='links'>
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {/* links-container class and div is important for it to work */}
+        {showLinks && (
+          <div className='links-container'>
+            <ul className='links'>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
